67eac033cd9befdc16af68985f0e2fbcb0cb97ae
mukae1997 <444652692@qq.com>
2019-09-24 12:32:21 +0800

    first commit

---
+++ LifeStateTimer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using UnityEngine;
+namespace Utility
+{
+    public class LifeStateTimer
+    {
+
+        private float state = 0;
+        private bool isDead = false;
+        private float startTime = 0;
+        private float lifeSpan;
+
+        public LifeStateTimer(float ls)
+        {
+            lifeSpan = ls;
+            reset();
+        }
+        public bool IsDead()
+        {
+            return isDead;
+        }
+
+        public void reset()
+        {
+            state = 0;
+            startTime = Time.time;
+            isDead = false;
+        }
+
+        public void Update()
+        {
+            if (isDead) return;
+            state = (Time.time - startTime)  / lifeSpan;
+            isDead = state > 1;
+            state = Mathf.Min(state, 1);
+        }
+
+        public float getState()
+        {
+            return state;
+        }
+    }
+
+}
---
+++ Utils.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Utility
+{
+    public static class Utils
+    {
+        public static Vector3 CopyVector(Vector3 v)
+        {
+            return new Vector3(v.x, v.y, v.z);
+        }
+        public static Quaternion CopyQuaternion(Quaternion v)
+        {
+            return new Quaternion(v.x, v.y, v.z, v.w);
+        }
+
+        public static void SetLocalPos(Transform t, Vector3 newpos, Vector3 axis)
+        {
+            Vector3 oldp = t.localPosition;
+            t.localPosition = new Vector3(
+                oldp.x * (1-axis.x) + axis.x * newpos.x,
+                oldp.y * (1 - axis.y) + axis.y * newpos.y,
+                oldp.z * (1 - axis.z) + axis.z * newpos.z);
+        }
+
+        
+        public static string[] LoadStrings(string fileName)
+        {
+            string[] strs = System.IO.File.ReadAllLines(fileName);
+        
+
+            return strs;
+        }
+
+
+    }
+
+}
